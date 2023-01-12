229f096aab0c38ea831ee3bc47dab397ae8e8072
Kleiton Batista <kleitonbatista@192.168.1.11>
2023-01-12 02:24:48 -0300

    salvando os dados inicias do produto, de forma bruta sem tratativas

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,6 +1,8 @@
 import { Button, TextField } from "@mui/material";
 import { useContext, useState } from "react";
+import { toast } from "react-toastify";
 import { AuthContext } from "../../contexts/auth";
+import firebase from "../../service/firebaseConnection";
 import "./style.css";
 export default function Dashboard() {
   const { signOut } = useContext(AuthContext);
@@ -14,6 +16,38 @@ export default function Dashboard() {
   const [sugestaoMin, setSugestaoMin] = useState(0);
   const [sugestaoMax, setSugestaoMax] = useState(0);
   const [numeracao, setNumeracao] = useState(0);
+
+  function calculaTotal(){
+      if(valor > 0 && quantidade > 0){
+        setTotal(valor * quantidade);
+      }
+  }
+  function calculaValorVenda(){
+      if(valor > 0 && percentaul > 0){
+          console.log(percentaul/100);
+        setValorVenda(valor + (valor * (percentaul / 100)));
+      }
+  }
+  function salvar (){
+      firebase.firestore().collection("produtos").add({
+        codigo : codigo,
+        descricaoProduto : produto,
+        quantidade : quantidade,
+        valor : valor,
+        total: total,
+        percentual: percentaul,
+        valorVenda: valorVenda,
+        sugestaoOne : sugestaoMin,
+        sugestaoTwo : sugestaoMax,
+        numeracao: numeracao
+      }).then((res)=>{
+          console.log(res);
+          toast.success("salvo com sucesso! "+res.id);
+      }).catch((err)=>{
+          console.log("erro");
+          console.log(err);
+      });
+  }
   return (
     <div className="container">
       <div className="form">
@@ -22,17 +56,17 @@ export default function Dashboard() {
           <TextField type="text"  label="Produto" onChange={(e)=>setProduto(e.target.value)} value={produto} className="col-md-12" id="outlined-basic"  variant="standard"/>
           <div className="area-valor">
             <TextField type="number" label="Quantidade" onChange={(e)=>setQuantidade(e.target.value)} value={quantidade} className="col-md-12" id="outlined-basic"  variant="standard"/>
-            <TextField type="text" label="Valor" onChange={(e)=>setValor(e.target.value)} value={valor} className="area-valor-space" id="outlined-basic"  variant="standard"/>
+            <TextField type="text" label="Valor" onChange={(e)=>{setValor(e.target.value)}} onKeyUp={calculaTotal} value={valor} className="area-valor-space" id="outlined-basic"  variant="standard"/>
             <TextField type="text"label="Total" onChange={(e)=>setTotal(e.target.value)} value={total} className="area-valor-space" id="outlined-basic"  variant="standard"/>
           </div>
           <div className="area-valor-final">
-            <TextField type="text"  label="Percentual %" onChange={(e)=>setPercentual(e.target.value)} value={percentaul} className="col-md-12" id="outlined-basic"  variant="standard"/>
+            <TextField type="text"  label="Percentual %" onChange={(e)=>setPercentual(e.target.value)} onKeyUp={calculaValorVenda} value={percentaul} className="col-md-12" id="outlined-basic"  variant="standard"/>
             <TextField type="text" label="Valor de Venda" onChange={(e)=>setValorVenda(e.target.value)} value={valorVenda} className="col-md-12" id="outlined-basic"  variant="standard"/>
             <TextField type="text" label="Valor Sugerido" onChange={(e)=>setSugestaoMin(e.target.value)} value={sugestaoMin} className="col-md-12" id="outlined-basic"  variant="standard"/>
             <TextField type="text" label="Valor Sugerido 2" onChange={(e)=>setSugestaoMax(e.target.value)} value={sugestaoMax} className="col-md-12" id="outlined-basic"  variant="standard"/>
          </div>
           <TextField type="number" label="Numeração" onChange={(e)=>setNumeracao(e.target.value)} value={numeracao} className="col-md-12" id="outlined-basic"  variant="standard"/>
-          <Button className="btn-salvar" variant="outlined">Salvar</Button>
+          <Button onClick={salvar} className="btn-salvar" variant="outlined">Salvar</Button>
           </div>
         
       </div>
